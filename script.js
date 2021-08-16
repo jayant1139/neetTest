a354f9f925ae1dfa1a520c4423e7d039ca48e6b2
Jayant <jayantsingh331@gmail.com>
2021-08-16 20:59:41 +0530

    updatescript

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const faq1=document.getElementById('faq1');
-const faq2=document.getElementById('faq2');
-const faq3=document.getElementById('faq3');
-const faq4=document.getElementById('faq4');
-const faq5=document.getElementById('faq5');
-const faq6=document.getElementById('faq6');
-const faq7=document.getElementById('faq7');
-const faq8=document.getElementById('faq8');
-const faq9=document.getElementById('faq9');
-const faq10=document.getElementById('faq10');
+
+
+const fa1=document.getElementById('fa1');
+const fa2=document.getElementById('fa2');
+const fa3=document.getElementById('fa3');
+const fa4=document.getElementById('fa4');
+const fa5=document.getElementById('fa5');
+const fa6=document.getElementById('fa6');
+const fa7=document.getElementById('fa7');
+const fa8=document.getElementById('fa8');
+const fa9=document.getElementById('fa9');
+const fa10=document.getElementById('fa10');
 
 const item1=document.getElementById('i1');
 const item2=document.getElementById('i2');
@@ -19,6 +21,153 @@ const item7=document.getElementById('i7');
 const item8=document.getElementById('i8');
 const item9=document.getElementById('i9');
 const item10=document.getElementById('i10');
-faq1.addEventListener('click',()=>{
-    
-});
+
+const icon1=document.getElementById('icon1');
+const icon2=document.getElementById('icon2');
+const icon3=document.getElementById('icon3');
+const icon4=document.getElementById('icon4');
+const icon5=document.getElementById('icon5');
+const icon6=document.getElementById('icon6');
+const icon7=document.getElementById('icon7');
+const icon8=document.getElementById('icon8');
+const icon9=document.getElementById('icon9');
+const icon10=document.getElementById('icon10');
+
+
+
+
+
+
+
+
+
+
+
+function displaychnge1(){
+    if(item1.style.display==="none"){
+        // icon1.removeClass('fa-plus');
+        
+        // icon1.addClass('fa-minus');
+        item1.style.display="block";
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            
+        } 
+        else if(item1.style.display="block"){
+            item1.style.display="none";
+            // icon1.addClass('fa-plus');
+            // icon1.removeClass('fa-minus');
+        }
+};
+function displaychnge2(){
+    if(item2.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item2.style.display="block";
+        } 
+        else {
+            item2.style.display="none";
+        }
+};
+function displaychnge3(){
+    if(item3.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item3.style.display="block";
+        } 
+        else if(item3.style.display="block"){
+            item3.style.display="none";
+        }
+};
+function displaychnge3(){
+    if(item3.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item3.style.display="block";
+        } 
+        else if(item3.style.display="block"){
+            item3.style.display="none";
+        }
+};
+function displaychnge4(){
+    if(item4.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item4.style.display="block";
+        } 
+        else if(item4.style.display="block"){
+            item4.style.display="none";
+        }
+};
+function displaychnge5(){
+    if(item5.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item5.style.display="block";
+        } 
+        else if(item5.style.display="block"){
+            item5.style.display="none";
+        }
+};
+function displaychnge6(){
+    if(item6.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item6.style.display="block";
+        } 
+        else if(item6.style.display="block"){
+            item6.style.display="none";
+        }
+};
+function displaychnge7(){
+    if(item7.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item7.style.display="block";
+        } 
+        else if(item7.style.display="block"){
+            item7.style.display="none";
+        }
+};
+function displaychnge8(){
+    if(item8.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item8.style.display="block";
+        } 
+        else if(item8.style.display="block"){
+            item8.style.display="none";
+        }
+};
+function displaychnge9(){
+    if(item9.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item9.style.display="block";
+        } 
+        else if(item9.style.display="block"){
+            item9.style.display="none";
+        }
+};
+function displaychnge10(){
+    if(item10.style.display==="none"){
+        
+        //   document.getElementsByClassName('plusminus').addEventListener('click',replacement());
+            item10.style.display="block";
+        } 
+        else if(item10.style.display="block"){
+            item10.style.display="none";
+        }
+};
+fa1.addEventListener('click',displaychnge1);
+fa2.addEventListener('click',displaychnge2);
+fa3.addEventListener('click',displaychnge3);
+fa4.addEventListener('click',displaychnge4);
+fa5.addEventListener('click',displaychnge5);
+fa6.addEventListener('click',displaychnge6);
+fa7.addEventListener('click',displaychnge7);
+fa8.addEventListener('click',displaychnge8);
+fa9.addEventListener('click',displaychnge9);
+fa10.addEventListener('click',displaychnge10);
+
+
+  
