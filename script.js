ec7abf6fa75c7ba5a2bf2a12c0abf9bc2d4e529a
Jayant <jayantsingh331@gmail.com>
2021-08-16 14:30:47 +0530

    testing

---
+++ script.js
@@ -0,0 +1,24 @@
+const faq1=document.getElementById('faq1');
+const faq2=document.getElementById('faq2');
+const faq3=document.getElementById('faq3');
+const faq4=document.getElementById('faq4');
+const faq5=document.getElementById('faq5');
+const faq6=document.getElementById('faq6');
+const faq7=document.getElementById('faq7');
+const faq8=document.getElementById('faq8');
+const faq9=document.getElementById('faq9');
+const faq10=document.getElementById('faq10');
+
+const item1=document.getElementById('i1');
+const item2=document.getElementById('i2');
+const item3=document.getElementById('i3');
+const item4=document.getElementById('i4');
+const item5=document.getElementById('i5');
+const item6=document.getElementById('i6');
+const item7=document.getElementById('i7');
+const item8=document.getElementById('i8');
+const item9=document.getElementById('i9');
+const item10=document.getElementById('i10');
+faq1.addEventListener('click',()=>{
+    
+});
